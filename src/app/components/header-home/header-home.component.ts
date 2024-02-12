46df2c7a00777af31a5a4cbb5c56bac8312b2ac4
Marcele Eller <dev.marcele@gmail.com>
2024-02-12 18:51:58 -0300

    feat(header): componente header com animacao toggle completa

--- src/app/components/header-home/header-home.component.ts
+++ src/app/components/header-home/header-home.component.ts
@@ -1,12 +1,34 @@
 import { Component } from '@angular/core';
+import { BreakpointObserver, BreakpointState } from '@angular/cdk/layout';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { NgClass } from '@angular/common';
 
 @Component({
   selector: 'app-header-home',
   standalone: true,
-  imports: [],
+  imports: [ NgbModule, NgClass ],
   templateUrl: './header-home.component.html',
   styleUrl: './header-home.component.css'
 })
 export class HeaderHomeComponent {
+
+  isCollapsed = true;
+  className:string = '';
+
+    constructor(
+      private breakpointObserver: BreakpointObserver,
+    ) {
+      this.breakpointObserver.observe([
+        "(max-width: 991px)"
+      ]).subscribe((result: BreakpointState) => {
+        if (result.matches) {
+          this.isCollapsed = false;
+          this.className = 'btn-outline-secondary';
+        } else {
+          this.isCollapsed = true;
+          this.className = 'btn-primary';
+        }
+      });
+    }
 
 }
--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
+import { HeaderHomeComponent } from "../../components/header-home/header-home.component";
 
 @Component({
-  selector: 'app-home',
-  standalone: true,
-  imports: [],
-  templateUrl: './home.component.html',
-  styleUrl: './home.component.css'
+    selector: 'app-home',
+    standalone: true,
+    templateUrl: './home.component.html',
+    styleUrl: './home.component.css',
+    imports: [HeaderHomeComponent]
 })
 export class HomeComponent {
 
