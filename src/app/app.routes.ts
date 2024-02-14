6e9afd7a3ab8219e753aaf5cbf54465f4fd5808a
Antoniobarrosdecastro <antoniobarrosdecastro@gmail.com>
2024-02-14 17:24:20 -0300

    feat(login.component): foi implementado o login

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,10 +2,12 @@ import { Routes } from '@angular/router';
 import { HistoricoComponent } from './pages/historico/historico.component';
 import { HomeComponent } from './pages/home/home.component';
 import { InicialComponent } from './pages/inicial/inicial.component';
+import { LoginComponent } from './pages/login/login.component';
 
 export const routes: Routes = [
     { path: '', component: HomeComponent },
     { path: 'inicial', component: InicialComponent},
     { path: 'transacoes', component: HistoricoComponent },
+    { path: 'login', component: LoginComponent},
 ];
 
--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -1,12 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { HeaderVoltarComponent } from "../../components/header-voltar/header-voltar.component";
+import { FormsModule } from '@angular/forms';
 
 @Component({
-  selector: 'app-login',
-  standalone: true,
-  imports: [],
-  templateUrl: './login.component.html',
-  styleUrl: './login.component.css'
+    selector: 'app-login',
+    standalone: true,
+    templateUrl: './login.component.html',
+    styleUrl: './login.component.css',
+    imports: [HeaderVoltarComponent, FormsModule]
 })
 export class LoginComponent {
+[x: string]: any;
+    @Input()
+    senha!: string;
+    email!: string;
+
+    login() {
+        console.log("Senha:", this.senha);
+        console.log("Email:", this.email);       
+
+    
+
+    }
+
 
 }
