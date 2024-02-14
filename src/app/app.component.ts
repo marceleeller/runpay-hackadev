8f2506aa7ebddf0edc22900a760ded3ab00000ca
Erick Prado <erickprado0610@gmail.com>
2024-02-14 16:45:36 -0300

    feat(cadastro.component): inicio da contrucao da tela de cadastro

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,13 +4,14 @@ import { RouterOutlet } from '@angular/router';
 import { HomeComponent } from "./pages/home/home.component";
 import { InicialComponent } from './pages/inicial/inicial.component';
 import { HistoricoComponent } from './pages/historico/historico.component';
+import { CadastroComponent } from "./pages/cadastro/cadastro.component";
 
 @Component({
     selector: 'app-root',
     standalone: true,
     templateUrl: './app.component.html',
     styleUrl: './app.component.css',
-    imports: [CommonModule, RouterOutlet, HomeComponent, InicialComponent, HistoricoComponent]
+    imports: [CommonModule, RouterOutlet, HomeComponent, InicialComponent, HistoricoComponent, CadastroComponent]
 })
 export class AppComponent {
   title = 'runpay-hackadev';
--- src/app/pages/cadastro/cadastro.component.ts
+++ src/app/pages/cadastro/cadastro.component.ts
@@ -1,12 +1,21 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormGroup, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-cadastro',
   standalone: true,
-  imports: [],
+  imports: [ReactiveFormsModule, CommonModule],
   templateUrl: './cadastro.component.html',
   styleUrl: './cadastro.component.css'
 })
 export class CadastroComponent {
-
+  cadastro = new FormGroup ({
+    cpf: new FormGroup(''),
+    nome: new FormGroup(''),
+    email: new FormGroup(''),
+    senha: new FormGroup(''),
+    confirmacao_email: new FormGroup(''),
+    confirmacao_senha: new FormGroup('')
+  });
 }
