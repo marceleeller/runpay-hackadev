189a00192cc8c073fd440f96b92f3c9a7691c487
JaquelineAPSantos <jaque.aps@hotmail.com>
2024-02-14 12:56:58 -0300

    feat(header-voltar): implementação do Header-voltar

--- src/app/components/header-voltar/header-voltar.component.ts
+++ src/app/components/header-voltar/header-voltar.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-header-voltar',
@@ -8,5 +8,6 @@ import { Component } from '@angular/core';
   styleUrl: './header-voltar.component.css'
 })
 export class HeaderVoltarComponent {
-
+ @Input()
+  nomeDaPagina!: string;
 }
