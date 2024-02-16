0d67b7886479907ab7b776479706a9ed57ec8271
Kelly Pedroso <kellyabadipedroso@gmail.com>
2024-02-15 23:18:03 -0300

    Adicionando novos arquivos ao diretorio inicial

--- src/app/pages/inicial/inicial.component.ts
+++ src/app/pages/inicial/inicial.component.ts
@@ -1,15 +1,15 @@
 import { Component } from '@angular/core';
-import { ComponentBotaoComponent } from '../../components/component-botao/component-botao.component';}
+import { ComponentBotaoComponent } from "../../components/component-botao/component-botao.component";
 
 @Component({
-  selector: 'app-inicial',
-  standalone: true,
-  imports: [],
-  templateUrl: './inicial.component.html',
-  styleUrl: './inicial.component.css'
+    selector: 'app-inicial',
+    standalone: true,
+    templateUrl: './inicial.component.html',
+    styleUrl: './inicial.component.css',
+    imports: [ComponentBotaoComponent]
 })
 export class InicialComponent {
 [x: string]: any;
-saldo: number | null = null;
+saldo: number = 32000;
 
 }
