4e21b20477a8d2ae69129c711f5c39727f20725f
Kelly Pedroso <kellyabadipedroso@gmail.com>
2024-02-16 01:18:17 -0300

    Adicionando novos arquivos ao diretório inicial

--- src/app/pages/inicial/inicial.component.ts
+++ src/app/pages/inicial/inicial.component.ts
@@ -1,12 +1,14 @@
 import { Component } from '@angular/core';
 import { ComponentBotaoComponent } from "../../components/component-botao/component-botao.component";
+import { HeaderHomeComponent } from "../../components/header-home/header-home.component";
+import { FooterComponent } from "../../components/footer/footer.component";
 
 @Component({
     selector: 'app-inicial',
     standalone: true,
     templateUrl: './inicial.component.html',
     styleUrl: './inicial.component.css',
-    imports: [ComponentBotaoComponent]
+    imports: [ComponentBotaoComponent, HeaderHomeComponent, FooterComponent]
 })
 export class InicialComponent {
 [x: string]: any;
