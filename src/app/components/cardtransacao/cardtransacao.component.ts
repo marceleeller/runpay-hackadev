bb1511439290e1ca541599b02638eb3cba40c939
JaquelineAPSantos <jaque.aps@hotmail.com>
2024-02-16 11:12:38 -0300

    fix(cardtransacao): coloquei todas as informações referente ao card dentro dele

--- src/app/components/cardtransacao/cardtransacao.component.ts
+++ src/app/components/cardtransacao/cardtransacao.component.ts
@@ -1,6 +1,6 @@
 import { Component, Input, Output, EventEmitter } from '@angular/core';
 import { Transacao } from './../../../models/transacao.model';
-import { CommonModule } from '@angular/common';
+import { CommonModule, formatDate } from '@angular/common';
 
 @Component({
   selector: 'app-cardtransacao',
@@ -14,10 +14,38 @@ export class CardtransacaoComponent {
   transacoesExibidas: Transacao[] = [];
   @Output() carregarMais = new EventEmitter<void>();
 
+  ngOnInit(): void {
+    if (this.transacao) {
+      this.transacao.dataFormatada = this.formatarData(this.transacao.data);
+    }
+  }
+
   formatarValor(valor: number | undefined): string {
     if (valor === undefined) {
       return '';
     }
     return valor.toFixed(2).replace('.', ',');
+  }
+
+  formatarData(data: Date): string {
+    const hoje = new Date();
+    const ontem = new Date(hoje);
+    ontem.setDate(hoje.getDate() - 1);
+
+    if (this.isMesmoDia(data, hoje)) {
+      return 'Hoje';
+    } else if (this.isMesmoDia(data, ontem)) {
+      return 'Ontem';
+    } else {
+      return formatDate(data, 'dd MMM', 'en-US').toLowerCase();
+    }
+  }
+
+  isMesmoDia(data1: Date, data2: Date): boolean {
+    return (
+      data1.getDate() === data2.getDate() &&
+      data1.getMonth() === data2.getMonth() &&
+      data1.getFullYear() === data2.getFullYear()
+    );
   }
 }
