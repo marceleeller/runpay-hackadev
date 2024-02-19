946148930c5b3ab4b6b8bca91a6e1438fe695add
Marcele Eller <dev.marcele@gmail.com>
2024-02-19 17:40:07 -0300

    feat(contato): finalizacao estilizacao pagina contato

--- src/app/pages/contato/contato.component.ts
+++ src/app/pages/contato/contato.component.ts
@@ -1,14 +1,31 @@
 import { Component } from '@angular/core';
 import { HeaderHomeComponent } from "../../components/header-home/header-home.component";
 import { FooterComponent } from "../../components/footer/footer.component";
+import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { CommonModule } from '@angular/common';
 
 @Component({
     selector: 'app-contato',
     standalone: true,
     templateUrl: './contato.component.html',
     styleUrl: './contato.component.css',
-    imports: [HeaderHomeComponent, FooterComponent]
+    imports: [HeaderHomeComponent, FooterComponent, CommonModule, ReactiveFormsModule]
 })
 export class ContatoComponent {
+
+  formularioMensagem = new FormGroup({
+    nome: new FormControl('', Validators.required),
+    email: new FormControl('', [Validators.required, Validators.email]),
+    celular: new FormControl('', Validators.required),
+    cpf: new FormControl('', Validators.required),
+    mensagem: new FormControl('', Validators.required)
+  });
+
+  get nome(): FormControl {
+    return this.formularioMensagem.get('nome') as FormControl;
+  }
+  get email(): FormControl {
+    return this.formularioMensagem.get('email') as FormControl;
+  }
 
 }
