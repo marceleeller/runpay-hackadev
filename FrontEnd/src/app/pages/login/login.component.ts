f5e2cfd28e92065403f90883f49f779540ef3d36
Marcele Eller <dev.marcele@gmail.com>
2024-03-15 20:33:20 -0300

    refactor(login): mudancas iniciais

--- FrontEnd/src/app/pages/login/login.component.ts
+++ FrontEnd/src/app/pages/login/login.component.ts
@@ -4,13 +4,15 @@ import { HeaderVoltarComponent } from "../../components/header-voltar/header-vol
 import { FooterComponent } from "../../components/footer/footer.component";
 import { Router } from '@angular/router';
 import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { NgxMaskDirective, provideNgxMask } from 'ngx-mask';
 
 @Component({
     selector: 'app-login',
     standalone: true,
     templateUrl: './login.component.html',
     styleUrl: './login.component.css',
-    imports: [HeaderVoltarComponent, FooterComponent, CommonModule, ReactiveFormsModule]
+    providers: [provideNgxMask()],
+    imports: [HeaderVoltarComponent, FooterComponent, CommonModule, ReactiveFormsModule, NgxMaskDirective]
 })
 
 export class LoginComponent {
@@ -23,50 +25,44 @@ export class LoginComponent {
 
     // formulário
     formularioLogin = new FormGroup ({
-      email: new FormControl('', [
-        Validators.required,
-        Validators.email,
-      ]),
-      senha: new FormControl('', [
-        Validators.required,
-        Validators.minLength(6)
-      ])
+      cpf: new FormControl('', [Validators.required, Validators.minLength(11)]),
+      senha: new FormControl('', [Validators.required, Validators.minLength(6)])
     });
 
-    get emailDoUsuario() {
-      return this.formularioLogin.get('email');
+    get cpf() {
+      return this.formularioLogin.get('cpf');
     }
 
-    get senhaDoUsuario() {
+    get senha() {
       return this.formularioLogin.get('senha');
     }
 
     senhaIncorreta : boolean = false;
-    autenticar(): void {
-      const userList = JSON.parse(localStorage.getItem('userList') as string);
-      const email = this.formularioLogin.value.email;
-      const senha = this.formularioLogin.value.senha;
-      let userValido:any;
+    // autenticar(): void {
+    //   const userList = JSON.parse(localStorage.getItem('userList') as string);
+    //   const email = this.formularioLogin.value.email;
+    //   const senha = this.formularioLogin.value.senha;
+    //   let userValido:any;
 
-      userList.forEach((item:any) => {
-        if (item.email == email && item.senha == senha) {
-            userValido = {
-                nome: item.nome,
-                email: item.email,
-                senha: item.senha
-            }
-        }
-      });
+    //   userList.forEach((item:any) => {
+    //     if (item.email == email && item.senha == senha) {
+    //         userValido = {
+    //             nome: item.nome,
+    //             email: item.email,
+    //             senha: item.senha
+    //         }
+    //     }
+    //   });
 
-      if (userValido == undefined) {
-        this.senhaIncorreta = true
-        this.rotas.navigateByUrl('/login');
-      } else {
-        let token = Math.random().toString(16).substring(2) + Math.random().toString(16).substring(2)
-        localStorage.setItem('token', token);
-        localStorage.setItem('usuarioLogado', JSON.stringify(userValido))
-        this.rotas.navigateByUrl('/inicial');
-      }
+    //   if (userValido == undefined) {
+    //     this.senhaIncorreta = true
+    //     this.rotas.navigateByUrl('/login');
+    //   } else {
+    //     let token = Math.random().toString(16).substring(2) + Math.random().toString(16).substring(2)
+    //     localStorage.setItem('token', token);
+    //     localStorage.setItem('usuarioLogado', JSON.stringify(userValido))
+    //     this.rotas.navigateByUrl('/inicial');
+    //   }
 
-    }
+    // }
 }
