07b91cfd1c9c2567e410d75fcf54d709d6bd76a8
Marcele Eller <dev.marcele@gmail.com>
2024-03-18 08:57:19 -0300

    feat(transferencia): adiciona confirmacao de transferencia e validacoes

--- FrontEnd/src/app/components/modal-confirmartransferencia/modal-confirmartransferencia.component.ts
+++ FrontEnd/src/app/components/modal-confirmartransferencia/modal-confirmartransferencia.component.ts
@@ -1,12 +1,32 @@
-import { Component } from '@angular/core';
+import { CommonModule, CurrencyPipe } from '@angular/common';
+import { Component, Input, inject } from '@angular/core';
+import { FormGroup, ReactiveFormsModule } from '@angular/forms';
+import { NgbActiveModal } from '@ng-bootstrap/ng-bootstrap';
+import { FormatBRLPipe } from '../../pipes/currencyFormat.pipe';
 
 @Component({
   selector: 'app-modal-confirmartransferencia',
   standalone: true,
-  imports: [],
+  imports: [ReactiveFormsModule, CommonModule, FormatBRLPipe],
   templateUrl: './modal-confirmartransferencia.component.html',
   styleUrl: './modal-confirmartransferencia.component.css'
 })
 export class ModalConfirmartransferenciaComponent {
+  @Input({ required: true}) formularioTransferencia!: FormGroup;
 
+  activeModal = inject(NgbActiveModal);
+
+  @Input() nomeConta:string = '';
+  @Input() numeroConta:string = '';
+  @Input() valorTransferencia:string = '';
+
+  // get
+  getCampo(nomeCampo: string) {
+    return this.formularioTransferencia.get(nomeCampo);
+  }
+
+  transferir() {
+    console.log('Transferência realizada com sucesso!');
+    console.log(this.formularioTransferencia.value);
+  }
 }
--- FrontEnd/src/app/pages/transferencia/transferencia.component.ts
+++ FrontEnd/src/app/pages/transferencia/transferencia.component.ts
@@ -1,22 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { HeaderVoltarComponent } from "../../components/header-voltar/header-voltar.component";
-import { FormControl, FormGroup, FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { FormBuilder, FormControl, FormGroup, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
 import { CurrencyMaskModule} from 'ng2-currency-mask';
 import { NgxMaskDirective, provideNgxMask } from 'ngx-mask';
+import { ModalConfirmartransferenciaComponent } from '../../components/modal-confirmartransferencia/modal-confirmartransferencia.component';
+import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-transferencia',
   standalone: true,
   templateUrl: './transferencia.component.html',
   styleUrl: './transferencia.component.css',
-  imports: [HeaderVoltarComponent, CurrencyMaskModule, FormsModule, ReactiveFormsModule, NgxMaskDirective],
+  imports: [HeaderVoltarComponent, CurrencyMaskModule, FormsModule, ReactiveFormsModule, NgxMaskDirective, CommonModule],
   providers: [provideNgxMask()]
 })
 
 export class TransferenciaComponent {
+
 numeroConta: string = '12345';
 saldo: string = '12.000,00';
 formularioTransferencia!: FormGroup;
+
+constructor(private fb: FormBuilder){
+
+}
+  // Exibir modal
+  private modalService = inject(NgbModal);
 
 ngOnInit() {
   this.criarFormulario();
@@ -29,17 +39,37 @@ ngOnInit() {
 }
 
 criarFormulario(){
-  this.formularioTransferencia = new FormGroup ({
-    valor: new FormControl(''),
-    conta: new FormControl(''),
-    descricao: new FormControl(''),
-    senha: new FormControl('')
-  });
+  this.formularioTransferencia = this.fb.group({
+    valor: this.fb.control('', [Validators.required, Validators.min(0.01)]),
+    conta: this.fb.control('', [Validators.required, Validators.minLength(8)]),
+    descricao: this.fb.control('', Validators.maxLength(60)),
+    senha: this.fb.control('', [Validators.required, Validators.minLength(6)])
+  })
+}
+
+// get
+getCampo(nomeCampo: string) {
+  return this.formularioTransferencia.get(nomeCampo);
 }
 
 getDescricaoLength() {
-  const descricao = this.formularioTransferencia.get('descricao')?.value || '';
+  const descricao = this.getCampo('descricao')?.value || '';
   return descricao.length;
+}
+
+abrirModalConfirmacao() {
+  const modalRef = this.modalService.open(ModalConfirmartransferenciaComponent);
+  modalRef.componentInstance.formularioTransferencia = this.formularioTransferencia;
+  modalRef.componentInstance.nomeConta = 'Luis Otávio'; //conectar com o back
+  modalRef.componentInstance.numeroConta = this.formularioTransferencia.get('conta')?.value;
+  modalRef.componentInstance.valorTransferencia = this.formularioTransferencia.get('valor')?.value;
+}
+
+validarContaEValor() {
+  if (this.getCampo('conta')?.valid && this.getCampo('valor')?.valid) {
+    return true;
+  }
+  return false;
 }
 
 }
