e87f6fd606fa84a2e869c34c72ac9e4357042c1b
Isadora de Oliveira <isadoradeoliveirapinto@gmail.com>
2024-03-18 12:11:48 -0300

    Feat Data Annotation Validations in Models

--- BackEnd/Runpay.API/Runpay.API/Domains/Models/Endereco.cs
+++ BackEnd/Runpay.API/Runpay.API/Domains/Models/Endereco.cs
@@ -2,14 +2,31 @@
 
 namespace Runpay.API.Domain.Model;
 
-public class Endereco:Entity
+public class Endereco : Entity
 {
+    [Required(ErrorMessage = "O campo Rua é obrigatório.")]
+    [MaxLength(100, ErrorMessage = "O campo Rua deve ter no máximo 100 caracteres.")]
     public string Rua { get; set; } = null!;
+
+    [Required(ErrorMessage = "O campo Número é obrigatório.")]
     public int Numero { get; set; }
+
+    [Required(ErrorMessage = "O campo Bairro é obrigatório.")]
+    [MaxLength(100, ErrorMessage = "O campo Bairro deve ter no máximo 100 caracteres.")]
     public string Bairro { get; set; } = null!;
+
+    [Required(ErrorMessage = "O campo Cidade é obrigatório.")]
+    [MaxLength(100, ErrorMessage = "O campo Cidade deve ter no máximo 100 caracteres.")]
     public string Cidade { get; set; } = null!;
+
+    [Required(ErrorMessage = "O campo Estado é obrigatório.")]
+    [MaxLength(100, ErrorMessage = "O campo Estado deve ter no máximo 100 caracteres.")]
     public string Estado { get; set; } = null!;
+
+    [Required(ErrorMessage = "O campo CEP é obrigatório.")]
     public string Cep { get; set; } = null!;
+
+    [MaxLength(100, ErrorMessage = "O campo Complemento deve ter no máximo 100 caracteres.")]
     public string? Complemento { get; set; }
 
 }
--- BackEnd/Runpay.API/Runpay.API/Domains/Models/Transacao.cs
+++ BackEnd/Runpay.API/Runpay.API/Domains/Models/Transacao.cs
@@ -4,15 +4,26 @@ using System.ComponentModel.DataAnnotations;
 
 namespace Runpay.API.Domain.Model;
 
-public class Transacao:Entity
+public class Transacao : Entity
 {
+    [Required(ErrorMessage = "O campo Conta do Destinatário é obrigatório.")]
+    [MaxLength(100, ErrorMessage = "O campo Conta do Destinatário deve ter no máximo 100 caracteres.")]
     public string ContaDestinatario { get; set; } = null!;
+
+    [Required(ErrorMessage = "O campo Nome do Destinatário é obrigatório.")]
+    [MaxLength(250, ErrorMessage = "O campo Nome do Destinatário deve ter no máximo 250 caracteres.")]
     public string NomeDestinatario { get; set; } = null!;
+
+    [MaxLength(100, ErrorMessage = "O campo Conta do Destinatário deve ter no máximo 100 caracteres.")]
     public string? Descricao { get; set; }
     [Required]
+    [Range(0.01, double.MaxValue, ErrorMessage = "O campo Valor deve ser maior que zero.")]
     public decimal Valor { get; set; }
     [Required]
+
+    [Required(ErrorMessage = "O campo Tipo de Transação é obrigatório.")]
     public ETipoTransacao TipoTransacao { get; set; }
+
     [Required]
     public int? ContaId { get; set; }
     public virtual Conta Conta { get; set; } = null!;
